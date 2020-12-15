aed59901250f51aa8512f56629482289b7914e20
Marleah Puckett <marleah@berkeley.edu>
2020-12-15 15:35:06 -0500

    ucb home image removed

--- webpages/home_page.py
+++ webpages/home_page.py
@@ -6,7 +6,8 @@ from PIL import Image
 def page():
 	st.header("Home")
 	st.subheader("Team: Jonathan Wong, Michael Wu, Marleah Puckett, Christian Reyes, James Cheney")
+	st.write("""
+	University of California, Berkeley
+	""")
 	home_image = Image.open('homeimg.png')
 	st.image(home_image, caption= '"Float like a Cadillac, sting like a Beemer." - Lightning McQueen', use_column_width=True)
-	ucb_image = Image.open('UCB.png')
-	st.image(ucb_image, use_column_width=True)
