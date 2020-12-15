9eb04bcff27a587991bc978687e681cb1f230d31
Marleah Puckett <marleah@berkeley.edu>
2020-12-15 14:48:05 -0500

    testing intro page

--- webpages/introduction_page.py
+++ webpages/introduction_page.py
@@ -3,8 +3,10 @@ import numpy as np
 import pandas as pd
 
 def page():
+	st.header("Introduction")
+	st.subheader("Float like a Cadillac, sting like a Beemer - Lightning McQueen")
 	st.write("""
-	### Introduction Page
+	### Introduction
 	Here's our first attempt at using data to create a table:
 	""")
 
