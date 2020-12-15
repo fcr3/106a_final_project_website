b9511b14ff20fa96415117e0e16e5c135ceec144
Marleah Puckett <marleah@berkeley.edu>
2020-12-15 14:21:04 -0500

    temp

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import pandas as pd
 from webpages.home_page import page as home
 from webpages.team_page import page as team
 from webpages.project_page import page as project
+from webpages.temp_page import page as temppage
 
 st.title('ROAR: A 106A Final Project')
 
 def sidebar():
 	st.sidebar.title("Where do you want to go?")
 	app_mode = st.sidebar.selectbox("",
-	    ["Home", "Team", "Project"])
+	    ["Home", "Team", "Project", "temp"])
 	return app_mode
 
 
@@ -22,6 +23,8 @@ def mainpage(page_mode):
 		team()
 	if page_mode == 'Project':
 		project()
+	if page_mode == 'temp':
+		temppage()
 
 if __name__ == '__main__':
 	page_mode = sidebar()
