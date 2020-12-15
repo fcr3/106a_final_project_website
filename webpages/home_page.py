2d3f36f9d5e0b7f67e07525deb161fb18d00dfa3
Marleah Puckett <marleah@berkeley.edu>
2020-12-15 15:25:05 -0500

    intro page image

--- webpages/home_page.py
+++ webpages/home_page.py
@@ -4,34 +4,7 @@ import pandas as pd
 from PIL import Image
 
 def page():
+	st.header("Home")
+	st.subheader("Team: Jonathan Wong, Michael Wu, Marleah Puckett, Christian Reyes, James Cheney")
 	home_image = Image.open('homeimg.png')
 	st.image(home_image, caption= '"Float like a Cadillac, sting like a Beemer." - Lightning McQueen', use_column_width=True)
-	st.write("""
-	## Home Page
-	Here's our first attempt at using data to create a table:
-	""")
-
-	df = pd.DataFrame({
-	  'first column': [1, 2, 3, 4],
-	  'second column': [10, 20, 30, 40]
-	})
-	df
-
-	st.write("""
-	Random chart data
-	""")
-
-	chart_data = pd.DataFrame(
-	     np.random.randn(20, 3),
-	     columns=['a', 'b', 'c'])
-
-	st.line_chart(chart_data)
-
-	map_data = pd.DataFrame(
-	    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-	    columns=['lat', 'lon'])
-
-	st.write("""
-	Random map data
-	""")
-	st.map(map_data)
