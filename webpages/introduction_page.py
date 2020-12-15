20d4cd1074a8c81c0a371090db70fb87bb238116
Marleah Puckett <marleah@berkeley.edu>
2020-12-15 14:43:13 -0500

    testing intro page

--- webpages/introduction_page.py
+++ webpages/introduction_page.py
@@ -4,32 +4,14 @@ import pandas as pd
 
 def page():
 	st.write("""
-	### Temp Page
+	### Introduction Page
 	Here's our first attempt at using data to create a table:
 	""")
-
-	df = pd.DataFrame({
-	  'first column': [1, 2, 3, 4],
-	  'second column': [10, 20, 30, 40]
-	})
-	df
 
 	st.write("""
 	Random chart data
 	""")
 
-	chart_data = pd.DataFrame(
-	     np.random.randn(20, 3),
-	     columns=['a', 'b', 'c'])
-
-	st.line_chart(chart_data)
-
-	map_data = pd.DataFrame(
-	    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-	    columns=['lat', 'lon'])
-
 	st.write("""
 	Random map data
 	""")
-	st.map(map_data)
-	st.video("https://www.youtube.com/watch?v=S3GxDf1XGBA")
